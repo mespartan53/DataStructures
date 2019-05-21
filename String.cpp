e8a0a4620b9e2ad1157e4ee735e3bb8c1c474ab2
mespartan53 <mespartan53@gmail.com>
2019-05-21 10:16:09 -0700

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************
+Marcus Loureiro - Estrada
+5/21/2019
+String.cpp
+
+Implementation of constructors and member functions defined in String.h.
+************************************************************************/
+
+#include "String.h"
+
+// default constructor
+String::String()
+{
+
+}
