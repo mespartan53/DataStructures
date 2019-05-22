713a6cb8675a86add4cb8600ef5263f5182fcd89
mespartan53 <mespartan53@gmail.com>
2019-05-22 13:37:20 -0700

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,3 +1,6 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
 #include <iostream>
 #include <cmath>
 
@@ -25,25 +28,67 @@ class Rectangle {
     void setBase(int b){base = b;}
     int getHeight(){return height;}
     int getBase(){return base;}
+    Rectangle & operator=(const Rectangle & r)
+    { 
+        base = r.base;
+        height = r.height;  
+        return *this;
+    }
     
     int getArea(){return base*height;}
     int getPerimeter(){return 2*base+2*height;}
     int getDiagonal(){return sqrt(static_cast<float>(base)*base+static_cast<float>(height)*height);}
+    bool isSquare(){return base == height;}
     
+    //output all rectangle data and draw it;
+    friend ostream & operator<<(ostream & out, Rectangle & r)
+    {
+     
+        if (r.isSquare())
+            out << "Square\n";
+        else
+            out <<"Rectangle\n";
+            
+        out << "Base: " << r.getBase() << endl
+            << "Height: " << r.getHeight() << endl
+            << "Perimeter: " << r.getPerimeter() << endl
+            << "Area: " << r.getArea() << endl
+            << "Diagonal: " << r.getDiagonal() << endl;
+            
+        for (int i = 0; i < r.getHeight(); i++)
+        {
+            for(int j = 0; j < r.getBase(); j++)
+            {  
+                if (i == 0 || i == r.getHeight() - 1)   
+                    out << "--";
+                else
+                    if (j == 0)
+                        out << "| ";
+                    else if (j == r.getBase() - 1)
+                        out << " |";
+                    else
+                        out <<"  ";
+            }
+            out << endl;
+        }
+        
+        return out;
+    }
+    
+    friend bool operator==(const Rectangle &left, const Rectangle &right)
+    { 
+        return (left.base * left.height) == (right.base * right.height);
+    }
+    
+    friend bool operator<(const Rectangle &left, const Rectangle &right)
+    {  
+        return (left.base * left.height) < (right.base * right.height);   
+    }
+    
+    friend bool operator<=(const Rectangle &left, const Rectangle &right)
+    {
+        return (left.base * left.height) <= (right.base * right.height);     
+    }
 };
 
-int main() {
-    
-    int base, height;
-    Rectangle * r1;
-    
-    cout << "Enter Base and Height split by a space and followed by enter.\n";
-    cin >> base >> height;
-    r1 = new Rectangle(base, height);
-    
-    cout << "Base: " << r1->getBase() << endl << "Height: " << r1->getHeight() << endl;
-    
-    system("Pause");
-    delete r1;
-    return 0;   
-}
+#endif
